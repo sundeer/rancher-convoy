14da2d085ceff92992d43ad98f2edb7d133c810f
sundeer <sundeer@rhsjmm.com>
2016-01-25 17:08:39 +0000

    test

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
-FROM rancher/os-console:v0.4.2
+FROM rancher/os-console:v0.4.0-rc10
 
 ADD convoy.tar.gz /tmp/convoy.tar.gz
+RUN cp -f /tmp/convoy.tar.gz/convoy/convoy* /
+RUN rm -rf /tmp/convoy.tar.gz
+
 ADD convoy-start.sh /convoy-start.sh
+RUN chmod +x /convoy-start.sh
 
-RUN cp /tmp/convoy.tar.gz/convoy/convoy* /usr/bin/ \
-    && rm -rf /tmp/convoy.tar.gz \
-    && chmod +x /convoy-start.sh
+RUN echo "unix:///var/run/convoy/convoy.sock" > /tmp/convoy.spec
 
-ENTRYPOINT ["/convoy-start.sh"]
+CMD ["/convoy-start.sh"]
--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-DOCKER_VOLUMES=/var/lib/docker/volumes
-DOCKER_PLUGINS=/etc/docker/plugins
-
-if [ -z $@ ]; then
-    if [ ! -d $DOCKER_PLUGINS ]; then
-        mkdir -p $DOCKER_PLUGINS
-    fi
-
-    echo "unix:///var/run/convoy/convoy.sock" > $DOCKER_PLUGINS/convoy.spec
-
-    CMD="daemon --drivers vfs --driver-opts vfs.path=$DOCKER_VOLUMES"
-else
-    CMD=$@
+if [ ! -d /var/lib/docker/convoy-volumes ]; then
+    mkdir -p /var/lib/docker/convoy-volumes
 fi
 
-exec /usr/bin/convoy $CMD
+if [ ! -d /etc/docker/plugins ]; then
+    mkdir -p /etc/docker/plugins
+fi
+
+if [ ! -d /var/run/convoy ]; then
+    mkdir -p /var/run/convoy
+fi
+
+if [ "$(cat /tmp/convoy.spec)" != "$(cat /etc/docker/plugins/convoy.spec)" ]; then
+    mv -f /tmp/convoy.spec /etc/docker/plugins/convoy.spec
+else
+    rm /tmp/convoy.spec
+fi
+
+ln -fs /convoy /usr/bin/convoy
+ln -fs /convoy-pdata_tools /usr/bin/convoy-pdata_tools
+
+/usr/bin/convoy daemon --drivers vfs --driver-opts vfs.path=/var/lib/docker/convoy-volumes
